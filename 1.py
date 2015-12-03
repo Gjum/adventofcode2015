6aa8e846473e43e9056d43928590b3b51d3560a8
Gjum <code.gjum@gmail.com>
2015-12-03 01:49:52 +0100

    oops, swapped 1st and 2nd part of day 1 in python

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 t=open("1.in").read()
 m={"(":1,")":-1}
 s=lambda i:sum(map(lambda c:m[c] if c in m else 0, t[:i]))
-print("1:", *filter(lambda x:x[0]==-1,map(lambda y:(s(y),y),range(len(t)))))
-print("2:", sum(map(lambda c:m[c] if c in m else 0, t)))
+print("1:", sum(map(lambda c:m[c] if c in m else 0, t)))
+print("2:", *filter(lambda x:x[0]==-1,map(lambda y:(s(y),y),range(len(t)))))
